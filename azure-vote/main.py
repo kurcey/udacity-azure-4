22e075538ba1b747192695e6197a334a593404d7
Kurt Wanliss <kurcey@yahoo.com>
2021-06-08 08:35:11 -0400

    added insites

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -5,32 +5,55 @@ import redis
 import socket
 import sys
 import logging
-from applicationinsights.logging import LoggingHandler
+#from applicationinsights.logging import LoggingHandler
 from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
-from applicationinsights import TelemetryClient
+# from applicationinsights import TelemetryClient
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # Logging
 #logger = # TODO: Setup logger
-handler = LoggingHandler('<YOUR INSTRUMENTATION KEY GOES HERE>')
-logging.basicConfig(handlers=[ handler ], format='%(levelname)s: %(message)s', level=logging.DEBUG)
+#handler = LoggingHandler('<YOUR INSTRUMENTATION KEY GOES HERE>')
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 # Metrics
 #exporter = # TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08')
 
 # Tracing
 #tracer = # TODO: Setup tracer
-tc = TelemetryClient('<YOUR INSTRUMENTATION KEY GOES HERE>')
-#tc.track_trace('My trace with context')
-#tc.flush()
+#tracer = TelemetryClient('<YOUR INSTRUMENTATION KEY GOES HERE>')
+#tracer.track_trace('My trace with context')
+#tracer.flush()
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08'),
+    sampler=ProbabilitySampler(1.0),
+)
 
 app = Flask(__name__)
 
 # Requests
 #middleware = # TODO: Setup flask middleware
-
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
@@ -68,12 +91,12 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tc.track_trace('Cat votes = ' + vote1)
+        tracer.span(name='Cate Vote')
         #print('Cat votes = ' + vote1)
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tc.track_trace('Dog votes = ' + vote2)
+        tracer.span(name='Dog vote')
         #print('Dog votes = ' + vote2)
 
         # Return index with values
