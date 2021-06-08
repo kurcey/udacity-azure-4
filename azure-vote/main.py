56f4fdc14037ac8744651522ce918d5cebd92457
Kurt Wanliss <kurcey@yahoo.com>
2021-06-08 12:51:10 -0400

    fix that stupid error from udacity

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
-# from applicationinsights import TelemetryClient
+from applicationinsights import TelemetryClient
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 from opencensus.ext.azure import metrics_exporter
 from opencensus.stats import aggregation as aggregation_module
@@ -18,45 +18,45 @@ from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
-from opencensus.ext.azure.trace_exporter import AzureExporter
-from opencensus.trace.samplers import ProbabilitySampler
-from opencensus.trace.tracer import Tracer
+#from opencensus.ext.azure.trace_exporter import AzureExporter
+#from opencensus.trace.samplers import ProbabilitySampler
+#from opencensus.trace.tracer import Tracer
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.ext.azure.log_exporter import AzureEventHandler
 
 # Logging
 #logger = # TODO: Setup logger
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08'))
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08'))
 logger.setLevel(logging.INFO)
 
 
 # Metrics
 #exporter = # TODO: Setup exporter
-exporter = metrics_exporter.new_metrics_exporter(
-  enable_standard_metrics=True,
-  connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08')
+#exporter = metrics_exporter.new_metrics_exporter(
+#  enable_standard_metrics=True,
+#  connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08')
 
 # Tracing
 #tracer = # TODO: Setup tracer
-#tracer = TelemetryClient('<YOUR INSTRUMENTATION KEY GOES HERE>')
-#tracer.track_trace('My trace with context')
-#tracer.flush()
-tracer = Tracer(
-    exporter=AzureExporter(
-        connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08'),
-    sampler=ProbabilitySampler(1.0),
-)
+tracer = TelemetryClient('InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
+tracer.track_trace('My trace with context')
+tracer.flush()
+#tracer = Tracer(
+#    exporter=AzureExporter(
+#        connection_string='InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08'),
+#    sampler=ProbabilitySampler(1.0),
+#)
 
 app = Flask(__name__)
 
 # Requests
 #middleware = # TODO: Setup flask middleware
-middleware = FlaskMiddleware(
-    app,
-    exporter=AzureExporter(connection_string="InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08"),
-    sampler=ProbabilitySampler(rate=1.0),
-)
+#middleware = FlaskMiddleware(
+#    app,
+#    exporter=AzureExporter(connection_string="InstrumentationKey=c902824f-d43b-42f6-8b71-ca0bf9dc4d08"),
+#    sampler=ProbabilitySampler(rate=1.0),
+#)
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
@@ -88,7 +88,8 @@ if not r.get(button2): r.set(button2,0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    tracer.span(name='hello')
+#    with tracer.span(name='hello'):
+#        print('Hello, World!')
   
 
     if request.method == 'GET':
@@ -96,13 +97,13 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span(name='Cat Vote')
+        #tracer.span(name='Cat Vote')
         logger.info('Cat Vote')
         #print('Cat votes = ' + vote1)
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span(name='Dog vote')
+        #tracer.span(name='Dog vote')
         logger.info('Dog vote')
         #print('Dog votes = ' + vote2)
 
@@ -125,7 +126,7 @@ def index():
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
             logger.info('Cat vote')
-            tracer.span('Cat vote')
+            #tracer.span('Cat vote')
             print('Cat votes = ' + vote1)
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -134,7 +135,7 @@ def index():
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
             logger.info('Dog vote')
-            tracer.span('Dog vote')
+            #tracer.span('Dog vote')
             print('Dog votes = ' + vote2)
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
